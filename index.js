93bf771b100f00570cde24783cf205d0af39a792
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-04-10 08:46:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//No.1
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
 
-// Write your code here
+//No.2
+Math.floor(Math.random() * 100) +1;
+
+//No.3
+let num3=104;
+let num4=20;
+let mod=num3%num4;
+
+
+
+//No.4
+Math.max(2, 5, 9, 20, 17, 12);
